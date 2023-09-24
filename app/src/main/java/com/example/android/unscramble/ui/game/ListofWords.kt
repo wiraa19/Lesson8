84b02198d491d6856d4f5d547a63c66b0cfe2b5c
wiranata <wiranatahawary19@gmail.com>
2023-09-24 14:18:45 +0700

    no message

---
+++ app/src/main/java/com/example/android/unscramble/ui/game/ListofWords.kt
@@ -0,0 +1,54 @@
+/*
+ * Copyright (C) 2020 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.android.unscramble.ui.game
+
+
+const val MAX_NO_OF_WORDS = 10
+const val SCORE_INCREASE = 10
+
+// List with all the words for the Game
+val allWordsList: List<String> =
+    listOf("banteng",
+        "merah",
+        "golkar",
+        "pdip",
+        "demokrat",
+        "gerindra",
+        "pan",
+        "psi",
+        "pks",
+        "golkar",
+        "presiden",
+        "dpr",
+        "dprd",
+        "mpr",
+        "waduh",
+        "hehe",
+        "bjir",
+        "indonesia",
+        "megawati",
+        "jokowidodo",
+        "puan",
+        "dayung",
+        "feri",
+        "wisnu",
+        "wira",
+        "haikal",
+        "akusiapa",
+        "siapa",
+        "igoy",
+        "david")
